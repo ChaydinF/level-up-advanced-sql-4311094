464915a1fdfd4667e135f0daa469cf2fea1448fd
ChaydinF <136875034+ChaydinF@users.noreply.github.com>
2024-02-28 19:40:53 +0000

    modified:   Challenges/Challenge3-1.sql

--- Challenges/Challenge3-1.sql
+++ Challenges/Challenge3-1.sql
@@ -1,10 +1,10 @@
 WITH cte AS (
-SELECT strftime('%Y', soldDate) AS soldYear, 
+SELECT strftime('%Y', soldDate) AS soldYear, --fomato de fechas sqlite
   salesAmount
 FROM sales
 )
 SELECT soldYear, 
-  FORMAT("$%.2f", sum(salesAmount)) AS AnnualSales
+  FORMAT("$%.2f", sum(salesAmount)) AS AnnualSales --formato de numero agrengando simbolo de dolares y 2 decimales despues del punto
 FROM cte
 GROUP BY soldYear
 ORDER BY soldYear
